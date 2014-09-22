a426dcaca9f18c19939b5208e90ae5a107603f06
qianfeng <qianfeng@qianfengdeMacBook-Pro-14.local>
2014-09-22 17:57:08 +0800

    Initial Commit

---
+++ 驴妈妈旅游/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  驴妈妈旅游
+//
+//  Created by qianfeng on 14-9-22.
+//  Copyright (c) 2014年 luoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
